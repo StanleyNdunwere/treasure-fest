cf458031dcfe07debc4c77723e2c2b18bf2ca60e
Stanley Ndunwere <StanleyNdunwere@gmail.com>
2020-12-07 12:49:23 +0100

    added a page to view all treasures

--- src/owned_treasures/possessed_treasures.component.jsx
+++ src/owned_treasures/possessed_treasures.component.jsx
@@ -2,12 +2,33 @@ import React from "react";
 import "../global/css/general_css.css";
 import "../treasure/treasure.css";
 import "./possessed.css";
+import { treasures } from "../global/treasures";
+import SingleTreasure from "../treasure/single_treasure.component";
+
 class PossessedTreasures extends React.Component {
   constructor(props) {
     super();
-    this.state = {};
+    this.state = {
+      treasuresPresent:
+        JSON.parse(window.localStorage.getItem("selectedTreasures")).length > 0,
+      treasureInfoArray: [],
+    };
   }
 
+  componentDidMount() {
+    let selectedTreasures = JSON.parse(
+      window.localStorage.getItem("selectedTreasures")
+    );
+    let treasureInfoArray = [];
+    for (let i = 0; i < selectedTreasures.length; i++) {
+      for (let j = 0; j < treasures.length; j++) {
+        if (treasures[j].shortName === selectedTreasures[i]) {
+          treasureInfoArray.push(treasures[j]);
+        }
+      }
+    }
+    this.setState({ treasureInfoArray: treasureInfoArray });
+  }
   render() {
     return (
       <div>
@@ -19,7 +40,21 @@ class PossessedTreasures extends React.Component {
           which thou have amassed so far and review their importance in your
           strategy. We trust that thy wisdom shall guide thee aright.
         </p>
-        <p>
+        {this.state.treasuresPresent ? (
+          <div className="treasure-container">
+            {this.state.treasureInfoArray.map((treasure) => {
+              return (
+                <SingleTreasure
+                  key={Math.random()}
+                  shortName={treasure.shortName}
+                  name={treasure.name}
+                  feature={treasure.feature}
+                  price={treasure.price}
+                />
+              );
+            })}
+          </div>
+        ) : (
           <div
             className="no-treasure-found"
             style={{
@@ -40,7 +75,7 @@ class PossessedTreasures extends React.Component {
               View Treasures You May Want To Possess
             </a>
           </div>
-        </p>
+        )}
       </div>
     );
   }
--- src/treasure_detail/treasure_detail.component.jsx
+++ src/treasure_detail/treasure_detail.component.jsx
@@ -1,48 +1,49 @@
-import React from 'react'
-import '../global/css/general_css.css'
-import { treasures } from '../global/treasures'
+import React from "react";
+import "../global/css/general_css.css";
+import { treasures } from "../global/treasures";
 
 class TreasureDetailComponent extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       treasure: treasures.filter((treasure) => {
-        return (treasure.shortName === this.props.match.params.treasure)
+        return treasure.shortName === this.props.match.params.treasure;
       }),
-  
-    }
-
+    };
   }
 
   render() {
     return (
       <div>
-        <h1>
-          Treasure Info
-        </h1>
+        <h1>Treasure Info</h1>
         <hr></hr>
-        <h2>
-          {this.state.treasure[0].name}
-        </h2>
-        <p>
-          {this.state.treasure[0].detail}
-        </p>
-        <p>
-          {"$" + this.state.treasure[0].price}
-        </p>
+        <h2>{this.state.treasure[0].name}</h2>
+        <p>{this.state.treasure[0].detail}</p>
+        <p>{"$" + this.state.treasure[0].price}</p>
         <input
           style={{
             border: "none",
           }}
-          type='submit' onClick={(e) => {
-            e.preventDefault()
-            window.localStorage.setItem("selectedTreasure", this.state.treasure[0].shortName)
-          }} className="button" name="submit" value="Possess At Once!" />
-
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            let selectedTreasures =
+              JSON.parse(window.localStorage.getItem("selectedTreasures")) ??
+              [];
+            selectedTreasures.push(this.state.treasure[0].shortName);
+            window.localStorage.setItem(
+              "selectedTreasures",
+              JSON.stringify(selectedTreasures)
+            );
+            this.props.history.push("/treasure-store")
+          }}
+          className="button"
+          name="submit"
+          value="Possess At Once!"
+        />
       </div>
-    )
+    );
   }
 }
 
-export default TreasureDetailComponent
+export default TreasureDetailComponent;
